a21b8a8b2eb6977d6370b52ce7c48f80995b2981
Ansh4490 <anshmishra2747@gmail.com>
2026-06-06 21:33:34 +0530

    Add files via upload

---
+++ DAY_10/CHARACTER_PYRAMID.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+   int i, j, n;
+   char ch;
+
+   printf("ENTER THE NUMBER OF ROWS\n ");
+   scanf("%d", &n);
+
+   for(i =1; i <= n; i++)
+   {
+     for(j = 1; j <= n - i; j++)
+     {
+       printf(" ");
+     }
+    
+     for(ch='A'; ch<'A'+i;ch++)
+       {printf("%c", ch);
+
+       }
+     for(ch='A'+i-2; ch>='A';ch--)
+     {
+       printf("%c", ch);
+     }
+     printf("\n");
+   }
+   return 0;
+}
---
+++ DAY_10/STAR_PYRAMID.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() 
+{
+    int n,i,j,k;
+
+    printf("ENETR THE NUMBER OF ROWS\n");
+    scanf("%d",&n);
+
+    for(i = 1; i <= n; i++) 
+    {
+
+        for(j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        } 
+
+        for(int k = 1; k <= (2 * i - 1); k++)
+        {
+            printf("*");
+        } 
+
+        printf("\n");
+    }
+
+    return 0;
+}
